e1c2ec279142b6928e0a6d5ef16134ba1dbd7fb5
Goode_Cheeseburgers <34482713+Cheeseborgers@users.noreply.github.com>
2025-02-22 23:57:00 +0000

    Updated licencing

--- libs/local/gouda_vulkan/include/gouda_vk_core.hpp
+++ libs/local/gouda_vulkan/include/gouda_vk_core.hpp
@@ -1,3 +1,19 @@
+// Copyright (c) 2025 GoodeCheeseburgers <https://github.com/Cheeseborgers>
+//
+// This file is part of gouda_vulkan.
+//
+// gouda_vulkan is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
 #pragma once
 
 #include <string_view>
